734751eafdb5524004f75e36dcebbf1e672d6201
Adrian <adriantoddross@gmail.com>
2018-04-10 11:12:47 -0400

    first commit

---
+++ binarySearchTree.js
@@ -0,0 +1,13 @@
+'use strict';
+
+class BinarySearchTree {
+  constructor(key=null, value=null, parent=null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+module.exports = BinarySearchTree;
