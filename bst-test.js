887783cf7a935200309b619720303873f010a268
Adrian <adriantoddross@gmail.com>
2018-04-10 11:55:17 -0400

    generate binary tree and take snapshots

---
+++ bst-test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/*
+Draw a BST
+Given the following data 3,1,4,6,9,2,5,7. If you were to insert this into an empty Binary Search Tree, how would the tree look like? (Draw the tree, no coding needed here).
+
+Took a snapshot.
+*/
+
+/*
+Remove the root
+Show how the tree would look like if you deleted the root. (Draw the tree, no coding needed here).
+
+2 becomes the root.
+*/
